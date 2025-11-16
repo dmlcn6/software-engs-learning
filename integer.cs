d467d00418916ebd7061d0c20ae70cfe71019f58
gramps <131563470+PaloSantoBreather@users.noreply.github.com>
2025-11-16 16:40:45 -0600

    removed unwanted line

--- integer.cs
+++ integer.cs
@@ -14,7 +14,6 @@ Console.WriteLine("Test");
 Console.WriteLine(name);
 Console.WriteLine($"Your IDE is currently: {name}");
 
-//int[] numbers = {1,2,3,4,5,6,7,8,9,10};
 int[] operations = {0,1,2,3};
 
 var rnd = new Random();
