6fce9617e6945f577f65dd81b5bfd267ded29d9c
Tyree <tyreebrown247@gmail.com>
2025-11-16 17:24:37 -0500

    CodingIntro01

---
+++ Week1Proj01.cs
@@ -0,0 +1,30 @@
+int TestNum01 = 1;
+string TestChar01 = "characters";
+
+
+
+int[] IntArray1 = [0,1,2,3,4,5,6,7,8,9,10];
+
+
+
+
+var Qa = 2;
+
+var Qb = 3;
+
+var a = Qa + Qb; // Addition
+var s = Qa + Qb; // Subtraction
+var m = Qa + Qb; // Mulltiplication
+var D = Qa + Qb; // Division
+
+
+var Eq = m;
+
+
+
+
+
+Console.WriteLine($"{Qa}+{Qb}={Eq}");
+
+
+
