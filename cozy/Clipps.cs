567336abbd33fc841e20fa4c6581d0832a9b43fe
gramps <131563470+PaloSantoBreather@users.noreply.github.com>
2025-11-18 21:13:28 -0600

    Readd files

---
+++ cozy/Clipps.cs
@@ -0,0 +1,9 @@
+int[] EvenNumbers = new int [5] {2,4,6,8,10};
+int[] OddNumbers = new int [5] {1,3,5,7,9};
+
+
+Console.WriteLine("Lets do some Math!");
+Console.WriteLine($"{EvenNumbers[2]} + {OddNumbers[2]} = {EvenNumbers[2] + OddNumbers[2]}");
+Console.WriteLine($"{EvenNumbers[0]} * {OddNumbers[4]} = {EvenNumbers[0] * OddNumbers[4]}");
+Console.WriteLine($"{EvenNumbers[4]} / {EvenNumbers[0]} = {EvenNumbers[4] / EvenNumbers[0]}");
+Console.WriteLine($"{OddNumbers[4]} - {OddNumbers[3]} = {OddNumbers[4] - OddNumbers[3]}");
