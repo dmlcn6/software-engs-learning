371a0e783bbc62158ccdfae08fc36ca6c52acdf9
Bryce Trent <brycet2011@gmail.com>
2025-11-19 00:08:34 -0500

    My Calculator Fucked Up lmao

---
+++ rex/integer.cs
@@ -0,0 +1,17 @@
+using system;
+
+int[10] numbers = new int[10] {0,1,2,3,4,5,6,7,8,9};
+
+
+public class calculator
+
+//accessModifier returnType FunctionName(parameterType1, parameterType2)
+{
+    public int Add(string a, string b, string c, string d);
+
+    string a = "+";
+    string b = "-";
+   new int c = "*";
+    new int d = "/";
+
+}
