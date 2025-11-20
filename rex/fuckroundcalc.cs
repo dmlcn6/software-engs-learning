7691f397b0e52e16c9bd15f259bd33e2b06f5c61
Bryce Trent <brycet2011@gmail.com>
2025-11-20 00:12:51 -0500

    still in progress added some new shit

---
+++ rex/fuckroundcalc.cs
@@ -0,0 +1,40 @@
+//AC - Acceptance Criteria
+// 4 funtions - add Div Subtract Multi
+//Display output to user
+// - Display equation
+// - Display divide by 0 error
+//2 point decimal outputs for division
+
+
+//accessModifier returnType FunctionName(parameterType1, parameterType2)
+
+namespace rexSimpleCalculator
+{   
+    
+    public class Integer
+    {
+        public var add()
+        {
+            string name = "+";
+        }
+        public var subtract()
+        {
+            string name = "-";
+        }
+        public var multiply()
+        {
+            string name = "*";
+        }
+        
+        public var divide()
+        {
+            string name = "/";
+        }
+
+
+    }
+
+}
+
+Console.Writeline("Still in deveolpement phase 1.0");
+
