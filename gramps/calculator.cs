c9ae545d6ba5575f82425f7699ef0dc4db660470
gramps <131563470+PaloSantoBreather@users.noreply.github.com>
2025-11-21 22:09:43 -0600

    we mightve broken thru

---
+++ gramps/calculator.cs
@@ -0,0 +1,72 @@
+//AC - Acceptance Criteria
+//  4 functions - Add Div Subtract Multi
+//  Display output to user
+//  - disply equation
+//  - display divide by 0 error
+//  2 point decimal outputs for division
+
+//  Show use of classes/functions to organize code
+
+using Test;
+
+var calc = new Calc();
+// Console.WriteLine(calc.name);  cant use this directly with dot notation (.) becuase it is private access modifier
+Console.WriteLine(calc.ReadName());
+
+calc.Add();
+calc.Add(1, 3);
+
+namespace Test
+{
+    public class Calc
+    {
+        Random randGenerator = new Random();
+        int rand1;
+        int rand2;
+        string name = "Calculator v1.0";
+ 
+        // THIS IS A CONSTRUCTOR
+        public Calc()
+        {
+            rand1 = randGenerator.Next(100);
+            rand2 = randGenerator.Next(100);
+        }
+
+        // functions are unique based on accessModifer returnType input paramters
+
+        public void Add(int number1, int number2)
+        {
+            LogAnswer($"{number1} * {number2} = {number1*number2}");
+        }
+
+        public void Add()
+        {
+            LogAnswer($"{rand1} * {rand2} = {rand1*rand2}");
+        }
+
+        public void Subtract(int number1, int number2)
+        {
+            LogAnswer($"{number1} * {number2} = {number1*number2}");
+        }
+        public void Divide(int number1, int number2)
+        {
+            LogAnswer($"{number1} * {number2} = {number1*number2}");
+        }
+        public void Multiply(int number1, int number2)
+        {
+            LogAnswer($"{number1} * {number2} = {number1*number2}");
+        }
+
+        private void LogAnswer(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+
+        public string ReadName()
+        {
+            return name;
+        }
+    }
+}
+
