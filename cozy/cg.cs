5e402bc5c311910fe7f9969a8ee81c0456e392e4
Albert <albharper2@gmail.com>
2025-12-02 22:28:50 -0600

    only the beginning

---
+++ cozy/cg.cs
@@ -0,0 +1,19 @@
+// AC for our work item
+// user must be identified by player name (input)
+// at least 5 prompts before one of the endings
+// 2 endings: Win or Lose
+// show use of constructors, static classes
+// 4 Object Oriented Programming principles (inheritance, encapsulation, poly, abstraction) 
+// player (with or without hp)
+// meter, counter, hp, progress tracking, 
+// combat/competition
+// Usable items in inventory
+public class CozyGame
+{
+    public static void Main()
+    {
+        Console.WriteLine("Let's play a game :)");
+    } 
+    
+       
+}
