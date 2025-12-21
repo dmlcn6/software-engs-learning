c4a9ceda0be09584c65a8807706c05ff467cde9a
Tyree <tyreebrown247@gmail.com>
2025-12-21 17:00:36 -0500

    Week 6 End Progression

--- wise/Week5Proj06.cs
+++ wise/Week5Proj06.cs
@@ -1,6 +1,16 @@
 
 
-
+//- **Sprint 2: 
+//- Tue 12/2: we are creating AC for our work item
+//- user must be identified by player name (input)
+//- at least 5 prompts before one of the endings
+//- 2 endings: Win or Lose//
+//- show use of constructors, static classes
+//- 4 Object Oriented Programming principles (inheritance, encapsulation, poly, abstraction) 
+//- player (with or without hp)
+//- meter, counter, hp, progress tracking, 
+//- combat/competition
+//- Usable items in inventory
 
 //Introduction to the game
 using System.Net.Quic;
