97c9ae681bc7236ccb8bde389cbca11e30417868
Albert <albharper2@gmail.com>
2026-01-21 21:50:28 -0600

    logger successful

--- cozy/Reup/Characters/obj/Debug/net10.0/characters.AssemblyInfo.cs
+++ cozy/Reup/Characters/obj/Debug/net10.0/characters.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("characters")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01c56c1898294e00b1eaa739530aad520bdadc15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15184729ee805849678dc2c573580937d05cffb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("characters")]
 [assembly: System.Reflection.AssemblyTitleAttribute("characters")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- cozy/Reup/Logger/obj/Debug/net10.0/Logger.AssemblyInfo.cs
+++ cozy/Reup/Logger/obj/Debug/net10.0/Logger.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Logger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e761d9c0145900a3c06f5b7794d498f3de5f31fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15184729ee805849678dc2c573580937d05cffb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Logger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Logger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
