fa93ccbda97a65be8e3037d4a1c1912bdd7d182d
Tyree <tyreebrown247@gmail.com>
2026-04-22 23:05:40 -0400

    4/22/26 [2] end of day commit + added javascript | hover over divs to change their class

---
+++ wise/UnitCC/Web-3/main.js
@@ -0,0 +1,17 @@
+const listItems = document.querySelectorAll("div");
+
+let previousClassName = "";
+
+function toggleDone(e) {
+    console.log("the previous class name was" + previousClassName);
+    if (e.target.className != "done") {
+        e.target.className = "done";
+    } else {
+        e.target.className = previousClassName;
+    }
+}
+
+listItems.forEach((item) => {
+    previousClassName = item.className;
+    item.addEventListener("mouseenter", toggleDone);
+});
