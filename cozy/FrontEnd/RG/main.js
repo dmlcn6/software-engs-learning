4272c21819a5440acd70cdf20ee7b79cd2c0a6f2
Albert <albharper2@gmail.com>
2026-05-07 21:57:31 -0500

    Forgot to add a couples of changes

---
+++ cozy/FrontEnd/RG/main.js
@@ -0,0 +1,5 @@
+import { generateRandomColor, generateRandomNumber } from "./board.js";
+
+let bets = document.querySelectorAll(".side-bets");
+let color = generateRandomColor();
+let number = generateRandomNumber();
