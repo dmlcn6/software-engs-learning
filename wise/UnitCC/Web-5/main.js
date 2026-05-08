6963ffb1bb06b7118967247f4a6fd4459a995554
Tyree <tyreebrown247@gmail.com>
2026-05-07 22:59:27 -0400

    05/07/26 added a live server for JS, and added the chip 1 png

--- wise/UnitCC/Web-5/main.js
+++ wise/UnitCC/Web-5/main.js
@@ -1,15 +1,35 @@
+import { generateRandomColor, generateRandomNumber } from "./board.js";
+
 const slotOptions = document.querySelectorAll(".slot");
 const categoryOptions = document.querySelectorAll(".category-Option");
 
 slotOptions.forEach((item) => {
-    item.addEventListener("mouseenter", selectionFeedback);
-    item.addEventListener("mouseleave", selectionreset)
+    item.addEventListener("mouseenter", selectionFeedbackSlot);
+    item.addEventListener("mouseleave", selectionResetSlot);
+});
+categoryOptions.forEach((item) => {
+    item.addEventListener("mouseenter", selectionFeedbackCategory);
+    item.addEventListener("mouseleave", selectionResetCategory);
 });
 
-function selectionFeedback(e) {
-    e.target.setAttribute('style', 'background-color: #a1a13e;');
+function selectionFeedbackSlot(e) {
+    e.target.setAttribute('style', 'background-color: #ffff21;');
 };
-
-function selectionreset(e) {
+function selectionResetSlot(e) {
     e.target.setAttribute('style', '');
 };
+
+
+function selectionFeedbackCategory(e) {
+    const categorySelection = e.target.classList[0];
+    if (item.classList.contains(categorySelection)) {
+        item.setAttribute('style', 'background-color: #ffff21;');
+    }
+
+}
+function selectionResetCategory(e) {
+
+}
+
+let color = generateRandomColor();
+let number = generateRandomNumber();
