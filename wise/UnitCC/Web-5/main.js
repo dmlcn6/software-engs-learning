da917ece925735e9c94e7620beed964b7f7858fd
Tyree <tyreebrown247@gmail.com>
2026-05-13 23:10:29 -0400

    5/13/26 Category hover feedback added, need to be clearner. NEXT work on chip drag/drop

--- wise/UnitCC/Web-5/main.js
+++ wise/UnitCC/Web-5/main.js
@@ -2,33 +2,43 @@ import { generateRandomColor, generateRandomNumber } from "./board.js";
 
 const slotOptions = document.querySelectorAll(".slot");
 const categoryOptions = document.querySelectorAll(".category-Option");
+const _1to12slots = document.querySelectorAll("._1to12Slots");
+const _13to24slots = document.querySelectorAll("._13to24Slots");
+const _25to36slots = document.querySelectorAll("._25to36Slots");
+
+
 
 slotOptions.forEach((item) => {
-    item.addEventListener("mouseenter", selectionFeedbackSlot);
-    item.addEventListener("mouseleave", selectionResetSlot);
+    item.addEventListener("mouseenter", HoverFeedbackSlot);
+    item.addEventListener("mouseleave", HoverResetSlot);
 });
 categoryOptions.forEach((item) => {
-    item.addEventListener("mouseenter", selectionFeedbackCategory);
-    item.addEventListener("mouseleave", selectionResetCategory);
+    item.addEventListener("mouseenter", HoverFeedbackCategory);
+    item.addEventListener("mouseleave", HoverResetCategory);
 });
 
-function selectionFeedbackSlot(e) {
+function HoverFeedbackSlot(e) {
     e.target.setAttribute('style', 'background-color: #ffff21;');
 };
-function selectionResetSlot(e) {
+function HoverResetSlot(e) {
     e.target.setAttribute('style', '');
 };
 
 
-function selectionFeedbackCategory(e) {
-    const categorySelection = e.target.classList[0];
-    if (item.classList.contains(categorySelection)) {
+function HoverFeedbackCategory(e) {
+    let objID = e.target.id;
+    const objSeeker = `.${objID}`
+
+    const objSeekerResults = document.querySelectorAll(objSeeker);
+    objSeekerResults.forEach((item) => {
         item.setAttribute('style', 'background-color: #ffff21;');
-    }
-
+    })
 }
-function selectionResetCategory(e) {
 
+function HoverResetCategory(e) {
+    objSeekerResults.forEach((item) => {
+        item.setAttribute('style', '');
+    })
 }
 
 let color = generateRandomColor();
