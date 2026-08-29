c75228829851396f71ed878ade90b88343351e76
Tyree <tyreebrown247@gmail.com>
2026-08-29 12:01:55 -0400

    added the array end of 8/29/26

--- wise/UnitCC/FullStackBeta/CalendarProj/src/App.tsx
+++ wise/UnitCC/FullStackBeta/CalendarProj/src/App.tsx
@@ -1,16 +1,48 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [username, setUsername] = useState('')
+// 1. break apart into components
+// 2. visuallize states for each component
+// 3. how does the data flow through the components
+
+function square({value, onSquareClick}: {value:string, onSquareClick: () => void}) {
+  function handleClick() {
+    onSquareClick();
+  }
 
   return (
-    <>
-      <div className="TopBar">
-        <h1>Welcome to my Calendar</h1>
-      </div>
-    </>
+    <button className="square" onClick={handleClick}> {value} </button>
   )
 }
 
-export default App
+
+
+
+
+function CalendarGrid() {
+  const [squares, setSquares] = useState(Array(30).fill("_"));
+
+
+  function handleClick(value: number) {
+    console.log(squares[0])
+  }
+
+
+  function App() {
+    const [username, setUsername] = useState('')
+
+    return (
+      <>
+        <div className="TopBarSection">
+          <h1>Welcome to my Calendar</h1>
+        </div>
+        <div className="MainSection">
+          <body>test</body>
+        </div>
+      </>
+    )
+  }
+  
+}
+
+export default square();
