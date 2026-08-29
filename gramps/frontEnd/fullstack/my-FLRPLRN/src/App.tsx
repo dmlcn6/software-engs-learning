418ad44d4bfa6cf69fe6efab0616ea9f958cb455
DLo <dLopezPrograms@gmail.com>
2026-08-29 11:00:25 -0500

    wrong page, right code

--- gramps/frontEnd/fullstack/my-FLRPLRN/src/App.tsx
+++ gramps/frontEnd/fullstack/my-FLRPLRN/src/App.tsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import {useState} from 'react';
 
 //1. identify your components
 //2. identify your behaviours/states
@@ -15,11 +16,24 @@ import './App.css'
  local storage api - save a user session once they have inputted the form 
  (even if the user has closed out and reopened the webpage)
 */
+
+// we want to start small, display one month
+
+let months = ['January','February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
+let monthsDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+
 function App() {
- 
+  const [monthIndex, setMonthIndex] = useState(7);
+  // duplicative - const [numberOfDays, setNumOfDays] = useState(7);
+
+  let month = months[monthIndex];
+  let days = monthsDays[monthIndex];
+
+  
 
   return (
-    <></>
+    <>
+    </>
   )
 }
 
