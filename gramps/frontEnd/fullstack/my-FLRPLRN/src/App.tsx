31832aa992a837f449a2b130277439e63933f9e2
DLo <dLopezPrograms@gmail.com>
2026-08-29 10:10:49 -0500

    added AC and thinking in react topics

--- gramps/frontEnd/fullstack/my-FLRPLRN/src/App.tsx
+++ gramps/frontEnd/fullstack/my-FLRPLRN/src/App.tsx
@@ -1,6 +1,20 @@
 
 import './App.css'
 
+//1. identify your components
+//2. identify your behaviours/states
+//3. identify your data flow between componenets
+
+
+/* AC
+ Use components
+ use arrays / list / objects
+ parent child nesting
+ use state and reducer / context
+ input form to take in a username
+ local storage api - save a user session once they have inputted the form 
+ (even if the user has closed out and reopened the webpage)
+*/
 function App() {
  
 
