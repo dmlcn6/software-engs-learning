6c22fb60406d09fa4d02216a429166838f4519e4
Albert <albharper2@gmail.com>
2026-09-01 22:22:21 -0500

    Made a static version of my calendar

--- cozy/FrontEnd/FullStack/cozy-app/src/App.tsx
+++ cozy/FrontEnd/FullStack/cozy-app/src/App.tsx
@@ -1,122 +1,75 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+//1. identify your components
+//2. identify your behaviours/states
+//3. identify your data flow between componenets
+// ^^^^^^^^ THATS ALL MOCK UP ^^^^^^^
+//4. build simple static version with mock data.  do not use state yet, only component props when needed
+//5. identify your minimal state data (minimal set of changing data that your app needs to remember)
+//6. identify where your state lives
 
-function App() {
-  const [count, setCount] = useState(0)
+
+/* AC
+ Use components
+ use arrays / list / objects
+ parent child nesting
+ use state and reducer / context
+ input form to take in a username
+ local storage api - save a user session once they have inputted the form 
+ (even if the user has closed out and reopened the webpage)
+*/
+
+const months = ['January','February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
+const monthsDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+const weekDays = ['Sun', 'Mon', 'Tues', 'Weds', 'Thurs', 'Fri', 'Sat']
+
+function Day ({weekDay, dayNumber}: {weekDay: string, dayNumber: number}) {
+  return (
+    <h3> {weekDay} {dayNumber}</h3>
+  )
+}
+
+function Weekdays() {
+  return (
+    <div className='daysOfWeek'>
+      {weekDays.map((day: string) => {
+        return (<p> {day} </p>)
+      })}
+    </div>
+  )
+}
+
+function Month({monthName, year}: {monthName: string, year: number}) {
+  return (
+    <h1>{monthName} {year}</h1>
+  )
+}
+
+function Calendar() {
+
+  const monthIndex = 7;
+  const monthName = months[monthIndex];
+  const days = monthsDays[monthIndex];
+  const year = 2026
+  const dayComponentsList = [];
+
+  for (let index = 0; index < days; index++) {
+    const dayComponent = <Day weekDay='' dayNumber={index + 1}></Day>;
+    dayComponentsList.push(dayComponent);
+    }
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      <Month monthName={monthName} year={year}></Month>
+      <Weekdays></Weekdays>
+      {dayComponentsList}
     </>
   )
 }
 
-export default App
+export default function App() {
+  return (
+    <>
+      <Calendar></Calendar>
+    </>
+  )
+
+}
